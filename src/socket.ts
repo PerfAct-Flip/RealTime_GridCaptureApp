0db877f1a4be5dba04eb243cb9e16533d98f3bf3
PerfAct-Flip <sohamsinha208@gmail.com>
2026-02-09 22:58:17 +0530

    changed socket import

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
-import dotenv from "dotenv";
-dotenv.config();
-export const socket = io(import.meta.env.VITE_SOCKET_URL, {
+
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL as string;
+
+export const socket = io(SOCKET_URL, {
   autoConnect: false,
 });
